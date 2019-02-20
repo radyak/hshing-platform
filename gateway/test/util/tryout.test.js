7ef9ae0553ac666d42ba2c3ba2aa1ab7f865bd0f
fvo <florian.vogel@iteratec.de>
2019-02-20 18:50:14 +0100

    Add async/await tryout tests

--- gateway/test/util/tryout.test.js
+++ gateway/test/util/tryout.test.js
@@ -2,6 +2,7 @@ var chai = require('chai')
 var expect = chai.expect
 
 describe('Try out', function () {
+
   it('path variable extraction', function () {
     var regex = new RegExp('/api/([a-zA-Z.-]*)/*(.*)', 'i')
 
@@ -12,5 +13,30 @@ describe('Try out', function () {
     match = regex.exec('/api/mongoclient')
     expect(match[1]).to.equal('mongoclient')
     expect(match[2]).to.equal('')
-  })
+  });
+
+  it("Using a Promise with async/await that resolves successfully", async function() {
+    
+    var testPromise = new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            resolve("works");
+        }, 200);
+    });
+
+    var result = await testPromise;
+
+    expect(result).to.equal("works");
+  });
+
+  it("Using a normal function with await works normally", async function() {
+    
+    var testPromise = function() {
+      return "works";
+    };
+
+    var result = await testPromise();
+
+    expect(result).to.equal("works");
+  });
+
 })
